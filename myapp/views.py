4511c6b68ad37283312fc6b4c90ff5e9caed7f1d
Cristiano Yang <cristianoyang@CristianoYangs.local>
2023-02-17 20:31:20 -0800

    Implement APIs in views.py to search restaurants nearby home.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+import googlemaps
+import requests
 
 # Create your views here.
+def search_restaurant_near_home(request, home_address):
+    gmaps = googlemaps.Client(key='AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc')
+    geocode_result = gmaps.geocode(home_address)
+    lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
+    
+    #compose the url
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat +"%2C" + lng + "&radius=1000&type=restaurant&keyword=restaurant&key=AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc"
+    response = requests.request("GET", url, headers = {}, data = {})
+    number_of_restaurants = len(response.json()['results'])
+    avg_rating = sum(e['rating'] for e in response.json()['results']) / number_of_restaurants
+    return (number_of_restaurants, avg_rating)
+    #Todo: return to rendering a result page and show current return data in that page.
+
+def search_hospital_near_home(request, home_address):
+    gmaps = googlemaps.Client(key='AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc')
+    geocode_result = gmaps.geocode(home_address)
+    lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
+    
+    #compose the url
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat +"%2C" + lng + "&radius=1000&type=hospital&keyword=hospital&key=AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc"
+    response = requests.request("GET", url, headers = {}, data = {})
+    number_of_hospitals = len(response.json()['results'])
+    avg_rating = sum(e['rating'] for e in response.json()['results']) / number_of_hospitals
+    return (number_of_hospitals, avg_rating)
+    #Todo: return to rendering a result page and show current return data in that page.
+
+def search_grocery_store_near_home(request, home_address):
+    gmaps = googlemaps.Client(key='AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc')
+    geocode_result = gmaps.geocode(home_address)
+    lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
+    
+    #compose the url
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat +"%2C" + lng + "&radius=1000&type=supermarket&keyword=supermarket&key=AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc"
+    response = requests.request("GET", url, headers = {}, data = {})
+    number_of_stores = len(response.json()['results'])
+    avg_rating = sum(e['rating'] for e in response.json()['results']) / number_of_stores
+    return (number_of_stores, avg_rating)
+    #Todo: return to rendering a result page and show current return data in that page.
+
+# def time_commuting_from_home_to_target(request, home_address, target_address):
+
