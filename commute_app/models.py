9f8334301cbf24465103c8e5aa73371f62d5b4a0
MoSeelan <mohana.seelan@gmail.com>
2023-02-22 12:15:53 -0800

    started adding model for search

--- commute_app/models.py
+++ commute_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Need to run these after adding any models:
+# python3 manage.py makemigrations 
+# python3 manage.py migrate
+
+class Commute_Search(models.Model):
+    start = models.TextField() # might need to be CharField?
+    end = models.TextField()
+    start_nick = models.CharField(max_length=50)
+    end_nick = models.CharField(max_length=50)
+    # add weights here later
--- commute_app/views.py
+++ commute_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import googlemaps
-import requests
+#import googlemaps
+#import requests
 
 # Create your views here.
 # request -> response
