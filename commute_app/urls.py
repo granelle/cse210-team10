2d267b2706f5094a0bb0a70b37af0ac47cdd5e9a
NellyZhou <1253336185@qq.com>
2023-03-06 18:28:09 -0800

    feat:basic login function

--- commute_app/urls.py
+++ commute_app/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.contrib import admin
+
 
 # URL config model
 # URLConf
@@ -7,5 +10,8 @@ urlpatterns = [
     path('home/', views.go_home, name = "home"),
     path('scores/', views.display_scores, name = "scores"),
     path('tutorial/', views.display_tutorial, name = 'tutorial'),
-    path('error/', views.display_error)
+    path('error/', views.display_error),
+    path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
