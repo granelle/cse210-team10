352654773233f34df4e61549e6468c5ff9d9e9af
w1jang <47434628+w1jang@users.noreply.github.com>
2023-03-16 14:04:47 -0700

    Clean up comments.

--- commute_app/views.py
+++ commute_app/views.py
@@ -27,19 +27,13 @@ from commute_app import models
 
 
 def go_home(request):
-    # pull data from db
-    # send email, etc
-    #return HttpResponse('Mohana says Hello world')
-    #return render(request, 'hello.html',{'name':'Ye'})
+    # Go to home page.
     return render(request, 'home.html')
 
 # Se-eun 2/22/23
 def display_scores(request):
     print(request.user.username)
     # display scores
-    # TODO: "go" button at home should direct to this page
-    # TODO: solve the incorrect address input -> jump to error page
-    #return render(request, 'rating.html')
     if request.method == 'POST':
         if request.POST['search_method'] == "search":
             inputContent = {
@@ -79,10 +73,7 @@ def display_scores(request):
 # Se-eun 2/22/23
 def display_scores(request):
     print(request.user.username)
-    # display scores
-    # TODO: "go" button at home should direct to this page
-    # TODO: solve the incorrect address input -> jump to error page
-    #return render(request, 'rating.html')
+    # Display scores for the score page.
     if request.method == 'POST':
         if request.POST['search_method'] == "search":
             inputContent = {
@@ -115,6 +106,7 @@ def display_scores(request):
             return render(request, 'rating.html', context = inputContent)
 
 def add_favorite_entry_to_database(request, input):
+    # Add favorite entry to database.
     if(request.user.is_authenticated):
         tmp = Search.objects.filter(startAdd =  input['home_address'], targetAdd=input['target_address'], username=request.user.username)
         if len(tmp) != 0:
@@ -127,49 +119,39 @@ def add_favorite_entry_to_database(request, input):
 
 
 def scores_generator(request, userInput):
-    # TODO: figure out the algorithm to generate score
-    # It's a basic scores calculate with only commute
-    #return search_near_home(request, home_address = userInput['start_addr'], 
-    #target_address = userInput['target_addr'], start_nickname = userInput['start_name'], target_nickname= userInput['target_name'])
+    # Generate scores for given start and target address.
     return search_near_home(request, weights_list= [userInput['commute_weight'], userInput['restaurant_weight'], userInput['grocery_weight'], userInput['medical_weight']], 
     start_address = userInput['start_addr'], 
     target_address = userInput['target_addr'], start_nickname = userInput['start_name'], target_nickname = userInput['target_name'], mode_list = userInput['mode_list'])
 
 # Xinyu 2/22/23
 def display_tutorial(request):
-    # display tutorial
-    # TODO: "about" button at home should direct to this page
-    # TODO: "get start" at this page should direct to home page
+    # display tutorial page.
     return render(request, 'tutorial.html')
 
 # Se-eun 3/10/23
 def display_contact(request):
-    # display tutorial
-    # TODO: "about" button at home should direct to this page
-    # TODO: "get start" at this page should direct to home page
+    # display contact page.
     return render(request, 'contact.html')
 
 def display_error(request):
-    # display error
-    # TODO: "go" button at home should direct to this page when errors occur
-    # TODO: "go back" button at this page should direct to home page 
+    # display error page.
     return render(request, 'error.html')
 
 def display_test(request):
-    # display error
-    # TODO: "go" button at home should direct to this page when errors occur
-    # TODO: "go back" button at this page should direct to home page 
+    # display error page.
     return render(request, 'test.html')
 
 def display_favorite(request):
+    # Display favorite page.
     search_list = Search.objects.filter(username=request.user.username).order_by('-overallScore')
-    #search_list = models.Search.objects.all()
     mydict = {
         'search_list':search_list
     }
     return render(request, 'favorite.html', context = mydict)
 
 class display_signup(generic.CreateView):
+    # Display signup page.
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
@@ -177,6 +159,7 @@ class display_signup(generic.CreateView):
 # Junyi backend work
 # Create your views here.
 def search_restaurant_near_home(gmaps, home_address):
+    # Search restaurants near home.
     geocode_result = gmaps.geocode(home_address)
     try:
         lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
@@ -190,9 +173,9 @@ def search_restaurant_near_home(gmaps, home_address):
         return (0, 0)
     avg_rating = sum(e['rating'] for e in response.json()['results']) / number_of_restaurants
     return (number_of_restaurants, avg_rating)
-    #Todo: return to rendering a result page and show current return data in that page.
 
 def search_hospital_near_home(gmaps, home_address):
+    # Search hosppitals near home.
     geocode_result = gmaps.geocode(home_address)
     try:
         lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
@@ -206,9 +189,9 @@ def search_hospital_near_home(gmaps, home_address):
         return (0, 0)
     avg_rating = sum(e['rating'] for e in response.json()['results']) / number_of_hospitals
     return (number_of_hospitals, avg_rating)
-    #Todo: return to rendering a result page and show current return data in that page.
 
 def search_grocery_store_near_home(gmaps, home_address):
+    # Search grocery stores near home.
     geocode_result = gmaps.geocode(home_address)
     try:
         lat, lng = str(geocode_result[0]['geometry']['location']['lat']), str(geocode_result[0]['geometry']['location']['lng'])
@@ -224,6 +207,8 @@ def search_grocery_store_near_home(gmaps, home_address):
     return (number_of_stores, avg_rating)
 
 def search_near_home(request, weights_list, start_address, target_address, start_nickname, target_nickname, mode_list=['driving', 'walking', 'bicycling', 'transit']):
+    # Commute score for score near home.
+    
     # Use default addresses if user input address is empty.
     if (start_address == ''):
         start_address= '3869 Miramar St, La Jolla, CA'
@@ -265,23 +250,11 @@ def search_near_home(request, weights_list, start_address, target_address, start
         'start_nickname': start_nickname,
         'target_nickname': target_nickname
     }
-    # Mohana save to search database here
-    #if(Search.objects.exists(startNick=start_nickname)):
-    # s1 = Search.objects.filter(startNick=start_nickname) # can use get?
-    # s1.overallScore = overall_info
-    # s1.driveScore = commuting_info
-    # s1.restScore = restaurant_info
-    # s1.hospScore = hospital_info
-    # s1.groceryScore = grocery_info
-    # s1.save()
-
-    #else:
-        #return render(request, 'error.html')
 
     return render(request, 'rating.html', context = context)
 
 def find_address_pair_in_database(request, start_addr, target_addr):
-# check if target address & start address exist in the database
+    # check if target address & start address exist in the database
     is_favorite = "unfav" 
     
     search_list = Search.objects.filter(username=request.user.username, startAdd = start_addr, targetAdd = target_addr)
@@ -289,13 +262,14 @@ def find_address_pair_in_database(request, start_addr, target_addr):
         is_favorite = "fav"
 
 def time_commuting_from_home_to_target(gmaps, source_address, target_address, mode):
+    # Commute time from home to target.
     s_geocode_result = gmaps.geocode(source_address)
     t_geocode_result = gmaps.geocode(target_address)
     try:
         s_lat, s_lng, t_lat, t_lng = str(s_geocode_result[0]['geometry']['location']['lat']), str(s_geocode_result[0]['geometry']['location']['lng']), str(t_geocode_result[0]['geometry']['location']['lat']), str(t_geocode_result[0]['geometry']['location']['lng'])
     except IndexError as e:
         raise e
-    # "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=YOUR_API_KEY"
+    
     result_list = []
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + s_lat + "%2C" + s_lng + "&destinations=" + t_lat + "%2C" + t_lng + "&key=AIzaSyDlgbzrdKouAchIHAfHog63OYtqkf0RPoc&mode=" + mode 
     response = requests.request("GET", url, headers = {}, data = {})
@@ -304,6 +278,7 @@ def time_commuting_from_home_to_target(gmaps, source_address, target_address, mo
     return (est_time, distance)
 
 def score_nearby_restaurants(gmaps, home_address):
+    # Commute score for nearby restaurants.
     try:
         num_of_restaurants, avg_rating = search_restaurant_near_home(gmaps, home_address)
     except IndexError as e:
@@ -312,6 +287,7 @@ def score_nearby_restaurants(gmaps, home_address):
     return min(5, round(score, 2))
 
 def score_nearby_hospitals(gmaps, home_address):
+    # Commute score for nearby hospitals.
     try:
         num_of_hospitals, avg_rating = search_hospital_near_home(gmaps, home_address)
     except IndexError as e:
@@ -320,6 +296,7 @@ def score_nearby_hospitals(gmaps, home_address):
     return min(5, round(score, 2))
 
 def score_nearby_stores(gmaps, home_address):
+    # Commute score for nearby stores.
     try:
         num_of_stores, avg_rating = search_grocery_store_near_home(gmaps, home_address)
     except IndexError as e:
@@ -328,6 +305,7 @@ def score_nearby_stores(gmaps, home_address):
     return min(5, round(score, 2))
 
 def score_commuting(gmaps, home_address, targe_address, mode):
+    # Commute score for commuting time.
     try:
         est_time, _ = time_commuting_from_home_to_target(gmaps, home_address, targe_address, mode)
     except IndexError as e:
